35299f058edb4b20194e0e7770b474239aed1580
Plínio Balduino <pbalduino@users.noreply.github.com>
2020-09-06 00:43:45 -0300

    Add comments to make ASM a slightly more readable

--- boot/boot.s
+++ boot/boot.s
@@ -3,24 +3,24 @@ bits 16
 global boot
 boot:
 	mov ax, 0x2401
-	int 0x15
+	int 0x15                      ; enable A20 bit
 
-	mov [disk],dl
+	mov [disk],dl                 ; disk is given by BIOS
 
-	mov ah, 0x2    ;read sectors
-	mov al, 6      ;sectors to read
-	mov ch, 0      ;cylinder idx
-	mov dh, 0      ;head idx
-	mov cl, 2      ;sector idx
-	mov dl, [disk] ;disk idx
-	mov bx, copy_target;target pointer
+	mov ah, 0x2                   ; read sectors
+	mov al, 6                     ; sectors to read
+	mov ch, 0                     ; cylinder idx
+	mov dh, 0                     ; head idx
+	mov cl, 2                     ; sector idx
+	mov dl, [disk]                ; disk idx
+	mov bx, copy_target           ; target pointer
 	int 0x13
-	cli
-	lgdt [gdt_pointer]
+	cli                           ; disable interruptions before we define GTD
+	lgdt [gdt_pointer]            ; load global descriptor table
 	mov eax, cr0
-	or eax,0x1
-	mov cr0, eax
-	mov ax, DATA_SEG
+	or eax,0x800000001            ; enable bit 1 of cr0 - protected mode
+	mov cr0, eax                  ; enable bit 31 of cr0 - memory paging
+	mov ax, DATA_SEG              ; point to the new code selector
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
@@ -28,16 +28,16 @@ boot:
 	mov ss, ax
 	jmp CODE_SEG:boot2
 gdt_start:
-	dq 0x0
+	dq 0x0                        ; first descriptor of GTD is null - 8 bytes
 gdt_code:
-	dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10011010b
-	db 11001111b
-	db 0x0
+	dw 0xffff                     ; segment length - 2 bytes
+	dw 0x0                        ; segment base - 2 bytes
+	db 0x0                        ; segment base - +1 byte
+	db 10011010b                  ; flags - 1 byte
+	db 11001111b                  ; flags (4b) + segment length (4b) - 1 byte
+	db 0x0                        ; segment base - 1 byte
 gdt_data:
-	dw 0xFFFF
+	dw 0xffff
 	dw 0x0
 	db 0x0
 	db 10010010b
@@ -52,8 +52,9 @@ disk:
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-times 510 - ($-$$) db 0
-dw 0xaa55
+times 510 - ($-$$) db 0        ; pad binary with zeroes
+dw 0xaa55                      ; magic number says BIOS the sector is bootloader
+
 copy_target:
 bits 32
 
