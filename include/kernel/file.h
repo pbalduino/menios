eb116d68c59f612493aded2791bfab2189d4a380
Plínio Balduino <pbalduino@users.noreply.github.com>
2023-10-31 07:45:16 -0300

    Scaffold to support file abstractions

---
+++ include/kernel/file.h
@@ -0,0 +1,8 @@
+#ifndef _KERNEL_FILE_H
+#define _KERNEL_FILE_H 1
+
+typedef struct {
+
+} file_descriptor;
+
+#endif
---
+++ src/kernel/file.c
@@ -0,0 +1,7 @@
+#include <kernel/file.h>
+
+static file_descriptor descriptors[0x100];
+
+void file_init() {
+  // file_descriptor
+}
