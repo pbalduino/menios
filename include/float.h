cf9d099b2edeff771d7792817232b97824b901ad
Plínio Balduino <32979+pbalduino@users.noreply.github.com>
2025-10-19 16:59:28 -0300

    Added float header for math build

---
+++ include/float.h
@@ -0,0 +1,40 @@
+#ifndef MENIOS_INCLUDE_FLOAT_H
+#define MENIOS_INCLUDE_FLOAT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FLT_RADIX        2
+
+#define FLT_MANT_DIG     24
+#define FLT_EPSILON      1.19209290e-7F
+#define FLT_DIG          6
+#define FLT_MIN_EXP      (-125)
+#define FLT_MIN          1.17549435e-38F
+#define FLT_MAX_EXP      128
+#define FLT_MAX          3.40282347e+38F
+
+#define DBL_MANT_DIG     53
+#define DBL_EPSILON      2.2204460492503131e-16
+#define DBL_DIG          15
+#define DBL_MIN_EXP      (-1021)
+#define DBL_MIN          2.2250738585072014e-308
+#define DBL_MAX_EXP      1024
+#define DBL_MAX          1.7976931348623157e+308
+
+#define LDBL_MANT_DIG    64
+#define LDBL_EPSILON     1.0842021724855044340e-19L
+#define LDBL_DIG         18
+#define LDBL_MIN_EXP     (-16381)
+#define LDBL_MIN         3.3621031431120935063e-4932L
+#define LDBL_MAX_EXP     16384
+#define LDBL_MAX         1.1897314953572317650e+4932L
+
+#define FLT_EVAL_METHOD  0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MENIOS_INCLUDE_FLOAT_H */
