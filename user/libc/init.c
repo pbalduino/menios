84240afcf9fbb3e6c054720b8f34bdfba9bfb511
Plínio Balduino <32979+pbalduino@users.noreply.github.com>
2025-10-27 16:14:51 -0300

    Flushed stdio buffers during libc shutdown

--- user/libc/init.c
+++ user/libc/init.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 
+#include <stdio.h>
 #include <stdlib.h>
 
 void __menios_env_init(int argc, char** argv, char** envp);
@@ -12,6 +13,7 @@ void __menios_init_libc(int argc, char** argv, char** envp) {
 
 void __menios_fini_libc(int status) {
   (void)status;
+  fflush(NULL);
   __menios_atexit_run();
   __menios_env_fini();
 }
